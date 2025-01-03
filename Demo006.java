f3c475c0fefe9a081a741174e4bca29fce97ef97
vardhini-0605 <kondabathulavardhini0@gmail.com>
2025-01-03 11:26:07 +0530

    Add files via upload

---
+++ Demo006.java
@@ -0,0 +1,12 @@
+
+public class Demo006 {
+    public static void main(String[] args){
+        String name = "vardhini";
+        String pwd = "vardhini0605";
+        while((name=="vardhini")&&(pwd=="vardhini0605")){
+            System.out.println("Hello welcome");
+            break;
+        }
+    }
+    
+}
