a5d897a53d6e4f37c3745ec957aab260ce18a9c6
vardhini-0605 <kondabathulavardhini0@gmail.com>
2025-01-25 12:50:55 +0530

    Swapping numbers

---
+++ Demo045.java
@@ -0,0 +1,23 @@
+public class Demo045 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        
+        a = a + b; 
+        b = a - b; 
+        a = a - b; 
+
+        
+        System.out.println("\nAfter swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
