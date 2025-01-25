93bfa91927f6fbbf697f33540c1dac4339d27791
vardhini-0605 <kondabathulavardhini0@gmail.com>
2025-01-25 12:58:56 +0530

    checking a number if palindrome

---
+++ Demo051.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Demo051 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int originalNumber = number; 
+        int reversed = 0;
+
+        
+        while (number != 0) {
+            int digit = number % 10; 
+            reversed = reversed * 10 + digit; 
+            number /= 10; 
+        }
+
+        
+        if (originalNumber == reversed) {
+            System.out.println(originalNumber + " is a palindrome.");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
