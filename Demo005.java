130c2231712ca9e5b9406a6873cfa02644facdae
vardhini-0605 <kondabathulavardhini0@gmail.com>
2025-01-27 12:13:52 +0530

    name and password

---
+++ Demo005.java
@@ -0,0 +1,10 @@
+public class Demo005{
+    public static void main(String[] args){
+        String name = "vardhini";
+        String pwd = "vardhini123";
+        while((name=="vardhini")&&(pwd=="vardhini123")){
+            System.out.println("Hello. Welcome");
+            break;
+        }
+    }
+}
