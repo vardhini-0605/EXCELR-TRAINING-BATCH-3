d627c09f7f8c8814ab9ede2df5741496c6bd6764
vardhini-0605 <kondabathulavardhini0@gmail.com>
2025-01-28 11:07:59 +0530

    Else If - Leap Year Calculator

---
+++ Demo022.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Demo022 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        if (year <= 0) {
+            System.out.println("Error: Year cannot be zero or negative.");
+        } else if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+        scanner.close();
+    }
+}
