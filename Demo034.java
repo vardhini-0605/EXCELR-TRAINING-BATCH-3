3ddbeed1ad3a68030057409ef09476bbc3df26f2
vardhini-0605 <kondabathulavardhini0@gmail.com>
2025-01-28 11:11:41 +0530

    Program to Check if a Number is Even or Odd

---
+++ Demo034.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Demo034 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even.");
+        } else {
+            System.out.println(num + " is Odd.");
+        }
+        
+        scanner.close();
+    }
+}
