843ad1047a958f514a54dae8d152243ff2358ff3
vardhini-0605 <kondabathulavardhini0@gmail.com>
2025-01-28 11:16:43 +0530

    Armstrong Number Checker for a Given Input

---
+++ Demo052.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Demo052 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int originalNumber = number;
+        int sum = 0;
+        int digits = String.valueOf(number).length();
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += Math.pow(digit, digits);
+            number /= 10;
+        }
+
+        if (sum == originalNumber) {
+            System.out.println(originalNumber + " is an Armstrong number.");
+        } else {
+            System.out.println(originalNumber + " is not an Armstrong number.");
+        }
+
+        scanner.close();
+    }
+}
