68583f7446e2ee441ce5ac5ee82d628de2ed04b1
vardhini-0605 <kondabathulavardhini0@gmail.com>
2025-01-29 12:57:41 +0530

    Merge Sorted Array from LeetCode

---
+++ LTCode008.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class LTCode008 {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m - 1, j = n - 1, k = m + n - 1; j >= 0; --k) {
+            nums1[k] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+        }
+    }
+
+    public static void main(String[] args) {
+      
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+     
+        merge(nums1, m, nums2, n);
+
+        
+        System.out.println("Merged Array: " + Arrays.toString(nums1));
+    }
+}
