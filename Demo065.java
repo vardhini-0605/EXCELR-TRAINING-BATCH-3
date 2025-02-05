2fc8a7ce87071b3dae9d509ce0cb075893da9e82
vardhini-0605 <kondabathulavardhini0@gmail.com>
2025-02-05 12:48:31 +0530

    print array elements

---
+++ Demo065.java
@@ -0,0 +1,10 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50}; 
+        
+        System.out.print("Array elements: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
